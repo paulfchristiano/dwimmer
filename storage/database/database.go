083b0f2a32a77ee14564cc00931363b20fb53bba
Paul Christiano <paulfchristiano@gmail.com>
2015-09-13 18:41:33 -0700

    added file backup/restore for database

--- storage/database/database.go
+++ storage/database/database.go
@@ -1,6 +1,11 @@
 package database
 
 import (
+	"bufio"
+	"encoding/json"
+	"fmt"
+	"os"
+
 	"gopkg.in/mgo.v2"
 	"gopkg.in/mgo.v2/bson"
 )
@@ -48,4 +53,41 @@ func (c C) Count() int {
 		panic(err)
 	}
 	return result
+}
+
+func (c C) Insert(data bson.M) {
+	c.collection.Insert(data)
+}
+
+func (c C) LoadFrom(s string) {
+	f, err := os.Open(s)
+	if err != nil {
+		panic(err)
+	}
+	defer f.Close()
+	r := bufio.NewReader(f)
+	for line, err := r.ReadSlice('\n'); err == nil; line, err = r.ReadSlice('\n') {
+		fmt.Println(line)
+		var data bson.M
+		err = json.Unmarshal(line, &data)
+		if err != nil {
+			panic(err)
+		}
+		c.Insert(data)
+	}
+}
+
+func (c C) DumpTo(s string) {
+	f, err := os.Create(s)
+	if err != nil {
+		panic(err)
+	}
+	defer f.Close()
+	for _, x := range c.All() {
+		out, err := json.Marshal(x)
+		if err != nil {
+			panic(err)
+		}
+		fmt.Fprintf(f, "%s\n", out)
+	}
 }
